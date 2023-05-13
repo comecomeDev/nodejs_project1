365c3febfdab72e420e13f47cefa4899840ea6db
comecomeDev <128262904+comecomeDev@users.noreply.github.com>
2023-05-13 16:25:21 +0900

    Create users.js

---
+++ nodejs_project2-3/models/users.js
@@ -0,0 +1,63 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      // models/users.js
+
+      // 1. Users 모델에서
+      this.hasMany(models.Posts, { // 2. Post 모델에게 1:N 관계 설정을 합니다.
+        sourceKey: 'userId', // 3. Users 모델의 userId 컬럼을
+        foreignKey: 'UserId', // 4. Posts  모델의 UserId 컬럼과 연결합니다.
+      });
+
+
+      this.hasMany(models.Comments, { 
+        sourceKey: 'userId', 
+        foreignKey: 'UserId', 
+      });
+    }
+  }
+  Users.init({
+    // nickname: DataTypes.STRING,
+    // password: DataTypes.STRING
+
+    userId: {
+      allowNull: false,
+      autoIncrement: true ,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    nickname: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue : DataTypes.NOW
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue : DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
